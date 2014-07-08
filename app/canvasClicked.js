2628c932c4e500281bdfacb40cd9bfcf992d815a
Hailey Petway <petwayh@gmail.com>
2014-07-07 20:42:34 -0700

    added some todos for optimization, and added some text styling to canvas.

--- app/canvasClicked.js
+++ app/canvasClicked.js
@@ -11,12 +11,14 @@ module.exports = function (canvasNumber, painted, turn, squaresFilled, content) 
   var cxt = c.getContext("2d");
 
   if(painted[canvasNumber-1] === false){
-    if(turn[0] % 2 === 0){                //Replace this as well.
+    if(turn[0] % 2 === 0){                //Todo: Replace this with a closure.
       cxt.beginPath();
       cxt.moveTo(10,10);
       cxt.lineTo(40,40);
       cxt.moveTo(40,10);
       cxt.lineTo(10,40);
+      cxt.strokeStyle = "#ff0000";
+      cxt.lineWidth = 3;
       cxt.stroke();
       cxt.closePath();
       content[canvasNumber-1] = "X";
@@ -24,15 +26,16 @@ module.exports = function (canvasNumber, painted, turn, squaresFilled, content) 
     else {
       cxt.beginPath();
       cxt.arc(25,25,20,0,Math.PI*2, true);
+      cxt.strokeStyle = "#ff0000";
+      cxt.lineWidth = 3;
       cxt.stroke();
       cxt.closePath();
       content[canvasNumber-1] = "0";
     }
-    turn[0] = turn[0] + 1;                //Replace this with a closure.
+    turn[0] = turn[0] + 1;                //Todo: Replace this with a closure.
     painted[canvasNumber-1] = true;
     squaresFilled[0] = squaresFilled[0] + 1;
     checkForWinners(content[canvasNumber-1], content);
-    console.log(squaresFilled);
 
     if(squaresFilled[0] === 9){
       window.alert("The Game Is Over!");
